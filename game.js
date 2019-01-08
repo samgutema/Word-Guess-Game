31dd3af1e56128debaf32fce806900cc8ede48c4
Samuel Gutema <samuel.gutema@gmail.com>
2019-01-08 03:47:35 -0500

    final revised version

--- game.js
+++ game.js
@@ -26,19 +26,20 @@
 //divs
 
 
-var currentWord = document.getElementById("current-word");
-var guessRemaining = document.getElementById("guess-remaining");
-var alreadyGuessed = document.getElementById("already-guessed");
-var winScore = document.getElementById("win-score");
-var loseScore = document.getElementById("lose-score");
+var $currentWord = document.getElementById("current-word");
+var $guessRemaining = document.getElementById("guess-remaining");
+var $alreadyGuessed = document.getElementById("already-guessed");
+var $winScore = document.getElementById("win-score");
+var $loseScore = document.getElementById("lose-score");
 
 
 //declaring game 
 var game = ["dog", "house", "cat", "laptop"];
+// var game = ["a b c d e f g h i j k l m n o p q r s t u v w x y z"];
 var wins = 0;
 var losses = 0;
 var attemptsLeft = 10;
-var gameRunning = true;
+var gameRunning = false;
 var pickedWord = " ";
 var pickedWordPlaceHolderArr = [];
 var guessLetterBank = [];
@@ -48,139 +49,68 @@ var incorrectLetterBank = [];
 
 
 
+// function newGame(){
+var gameRunning = true;
+var attemptsLeft = 10;
+var guessLetterBank = [];
+var pickedWordPlaceHolderArr = [];
+var incorrectLetterBank = [];
+
+//pick random new word
+pickedWord = game[Math.floor(Math.random() * game.length)];
+
+//create placeholder out of picked word 
+for (var i = 0; i < pickedWord.length; i++) {
+    if (pickedWord[i] === " ") {
+        pickedWordPlaceHolderArr.push(" ");
+    }
+    else {
+        pickedWordPlaceHolderArr.push("_");
+    }
+
+
+
+}
+$guessRemaining.textContent = attemptsLeft;
+$currentWord.textContent = pickedWordPlaceHolderArr.join(" ");
+$alreadyGuessed.textContent = incorrectLetterBank;
+
+
+function letterGuess(letter) {
+    // console.log(letter);
+
+    if (gameRunning === true && guessLetterBank.indexOf(letter) === -1) {
+        //run game here
+        incorrectLetterBank.push(letter);
+        attemptsLeft--;
+
+        if (attemptsLeft === 0) { alert("game over" + losses--) }
+        else if (pickedWord.toLocaleLowerCase() === pickedWordPlaceHolderArr.join("").toLocaleLowerCase()) { alert("winner" + wins++) }
+
+
+        for (var i = 0; i < pickedWord.length; i++) {
+            if (pickedWord[i].toLocaleLowerCase() === letter.toLocaleLowerCase()) {
+
+                pickedWordPlaceHolderArr[i] = pickedWord[i];
+            }
+        }
+
+        $currentWord.textContent = pickedWordPlaceHolderArr.join(" ");
+        $alreadyGuessed.textContent = incorrectLetterBank;
+        $guessRemaining.textContent = attemptsLeft;
+        $loseScore.textContent = losses;
+        $winScore.textContent = wins;
+
+
+    }
+
+}
+
 document.onkeyup = function (event) {
-    
-    var wins = 0;
-    var losses = 0;
-         // var attemptsLeft = 10;
- //  var gameRunning = true;
-        // var pickedWord = "";
-   var pickedWordPlaceHolderArr = [""];
-    var guessLetterBank = [""];
-        // var incorrectLetterBank = [];
 
- 
-
-
-    //pick random new word
-    pickedWord = game[Math.floor(Math.random() * game.length)];
-
-    //create placeholder out of picked word 
-    for (var i = 0; i < pickedWord.length; i++) {
-        if (pickedWord[i] === " ") {
-            pickedWordPlaceHolderArr.push(" ");
-        }
-        else {
-            pickedWordPlaceHolderArr.push("_");
-        }
-
-        
-    }
-    //write new game to the dom 
-    guessRemaining.textContent = attemptsLeft;
-    currentWord.textContent = pickedWordPlaceHolderArr.join(" ");
-    alreadyGuessed.textContent = incorrectLetterBank;
-
-
-
-
-    letterGuess = function (letter) {
-        console.log(letter);
-
-        if (gameRunning === true && guessLetterBank.indexOf(letter) === -1) {
-            //run game here
-            guessLetterBank.push(letter);
-
-            //check if guessed letter is in picked word 
-
-             for (var j = 0; j < pickedWord.length; j++) {
-               if (pickedWord[j].toLocaleLowerCase() === letter.toLocaleLowerCase()) {
-
-                    pickedWordPlaceHolderArr[j] = pickedWord[j];
-               }
-           }
-
-            currentWord.innerHTML = pickedWordPlaceHolderArr.join(" ");
-
-
-        }
-
-
-    }
-
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
+   // if (event.keyCode >= 65 && event.keyCode <= 90) {
         letterGuess(event.key);
 
-    }
-
-
-
-
-    checkIncorrect = function (letter) {
-       if (attemptsLeft === 0 ) {
-           attemptsLeft = 10  
-          
-         
-        }
-
-        pickedWordPlaceHolderArr.indexOf(letter.toLocaleLowerCase()) === -1
-        &&
-        pickedWordPlaceHolderArr.indexOf(letter.toLocalUpperCase()) === -1
-        attemptsLeft--;
-        incorrectLetterBank.push(letter);
-
-        alreadyGuessed.textContent = incorrectLetterBank.join(" ");
-        guessRemaining.textContent = attemptsLeft;
-
-
-
-    }
-
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-        checkIncorrect(event.key);
-
-   }
-
-
-
-    //check for loss
-
-    checkLoss = function () {
-        if (attemptsLeft === 0) {
-            losses++;
-
-            loseScore.textContent = losses;
-
-        }
-
-
-    }
-
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-        checkLoss(event.key);
-
-    }
-
-
-    //check for win 
-
-    checkWin = function () {
-        if (pickedWord.toLocaleLowerCase() === pickedWordPlaceHolderArr.join("").toLocaleLowerCase()) {
-            wins++;
-
-            winScore.textContent = wins;
-
-        }
-    }
-
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-        checkWin(event.key);
-
-    }
-
-
-
-   
-
+    //}
 }
 
